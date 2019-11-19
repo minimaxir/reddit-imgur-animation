03fe7fd9f261d2cf92abc5588faebe3af8a4738a
Max Woolf <max@minimaxir.com>
2019-11-18 21:26:53 -0800

    Final query

--- subreddit_image_domains.sql
+++ subreddit_image_domains.sql
@@ -1,11 +1,10 @@
-#standardSQL
 WITH top_image_subreddits AS (
   SELECT
     subreddit
   FROM
     `fh-bigquery.reddit_posts.*`
   WHERE
-    _TABLE_SUFFIX BETWEEN '2019_06' AND '2019_08'
+    _TABLE_SUFFIX BETWEEN '2019_01' AND '2019_08'
   GROUP BY
     subreddit
   HAVING
@@ -18,29 +17,30 @@ WITH top_image_subreddits AS (
   ORDER BY
     APPROX_COUNT_DISTINCT(author) DESC
   LIMIT
-    50 )
+    100 )
     
 SELECT
   DATE(TIMESTAMP_TRUNC(TIMESTAMP_SECONDS(created_utc), MONTH)) AS month,
   subreddit,
   CASE
-    WHEN REGEXP_CONTAINS(domain, 'reddituploads|redd.it') THEN "Reddit"
-    WHEN REGEXP_CONTAINS(domain, 'imgur') THEN "Imgur"
-    WHEN REGEXP_CONTAINS(domain, 'gfycat') THEN "Gfycat"
-    WHEN REGEXP_CONTAINS(domain, 'giphy') THEN "Giphy"
-    WHEN REGEXP_CONTAINS(domain, 'flickr') THEN "Flickr"
-    WHEN REGEXP_CONTAINS(domain, 'tumblr') THEN "Tumblr"
-    WHEN REGEXP_CONTAINS(domain, 'instagram') THEN "Instagram"
-    WHEN REGEXP_CONTAINS(domain, 'facebook') THEN "Facebook"
+    WHEN REGEXP_CONTAINS(domain, 'reddituploads.com|redd.it') THEN 'Reddit'
+    WHEN REGEXP_CONTAINS(domain, 'imgur') THEN 'Imgur'
+    WHEN REGEXP_CONTAINS(domain, 'gfycat') THEN 'Gfycat'
+    WHEN REGEXP_CONTAINS(domain, 'giphy') THEN 'Giphy'
+    WHEN REGEXP_CONTAINS(domain, 'flickr') THEN 'Flickr'
+    WHEN REGEXP_CONTAINS(domain, 'tumblr') THEN 'Tumblr'
+    WHEN REGEXP_CONTAINS(domain, 'instagram') THEN 'Instagram'
+    WHEN REGEXP_CONTAINS(domain, 'facebook') THEN 'Facebook'
   ELSE
-  "Other"
+  'Other'
 END
   AS website,
   COUNT(*) AS num_submissions
 FROM
   `fh-bigquery.reddit_posts.*`
 WHERE
-  _TABLE_SUFFIX BETWEEN '2019_06' AND '2019_08'
+  (_TABLE_SUFFIX BETWEEN '2016_01' AND '2019_08' 
+  OR _TABLE_SUFFIX = 'full_corpus_201512')
   AND subreddit IN (SELECT subreddit FROM top_image_subreddits)
 GROUP BY
   month,
@@ -50,3 +50,4 @@ ORDER BY
   month,
   subreddit,
   num_submissions DESC
+
